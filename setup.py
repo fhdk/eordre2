c76214f90eafd17abbf02ff0e1e2a3664efe7467
fhdk <fh@uex.dk>
2017-08-27 15:10:45 +0200

    routine from desktop

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='eordre-app.pyqt',
     version='0.2.1',
     packages=['', 'util', 'models', 'dialogs', 'resources', 'configuration'],
-    url='https://github.com/fhdk/eordre-app.pyqt.pyqt',
+    url='https://github.com/fhdk/eordre-app.pyqt',
     license='AGPL',
     author='Frede Hundewadt',
     author_email='fh@uex.dk',
-    description='Eordre app builders with python and pyqt',
+    description='Eordre app build with Python 3 and PyQt5',
     requires=['PyQt5']
 )
