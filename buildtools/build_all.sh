b0a6eba4ceda3de39f6c65458864bd2ff845c4a0
fhdk <fh@uex.dk>
2017-08-30 15:07:21 +0200

    routine from desktop

--- buildtools/build_all.sh
+++ buildtools/build_all.sh
@@ -5,7 +5,6 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 targetdir="../resources"
-echo ${targetdir}
 rm -f ${targetdir}/*_rc.py
 ./build_icons_rc.sh
 ./build_ui_rc.sh
